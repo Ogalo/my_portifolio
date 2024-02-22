f3119d626b063e2c7fda860353544daee012c3db
Ogalo <ogaloharrison@gmail.com>
2024-02-22 06:14:22 +0300

    Create a home page

---
+++ src/pages/home.tsx
@@ -0,0 +1,15 @@
+import { Typography, Stack, Box } from "@mui/material";
+
+
+const Home = () => {
+
+    return (
+        <Box>
+
+          Ogalo harry the big headed boy
+
+        </Box>
+    );
+};
+
+export default Home;
