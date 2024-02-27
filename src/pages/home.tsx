cc19deda5b74aed6eb254bcaca4f1a9953ea5a1f
Ogalo <ogaloharrison@gmail.com>
2024-02-27 08:17:18 +0300

    Add navigation bar to the home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,12 @@
 import { Typography, Stack, Box } from "@mui/material";
+import NavBar from '../components/NavBar';
 
 
 const Home = () => {
 
     return (
         <Box>
-
-          Ogalo harry the big headed boy
+          <NavBar />
 
         </Box>
     );
