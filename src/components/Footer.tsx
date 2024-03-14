db72a19d4d731358c82c9c044271c187a6d3400b
Ogalo <ogaloharrison@gmail.com>
2024-03-15 01:32:51 +0300

    Change the style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { Box, Typography, Stack, Button } from "@mui/material";
 const Footer = () => {
   return (
     <Box borderTop={1}>
-     <Typography variant='h6' fontFamily='karma' textAlign='center' py={1}>@2024 Harrison Otieno || Automate ventures. All rights reserved.</Typography>
+     <Typography fontFamily='karma' textAlign='center' py={1}>@2024 Harrison Otieno || Automate ventures. All rights reserved.</Typography>
     </Box>
   )
 }
