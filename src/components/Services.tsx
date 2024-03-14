bce2cf73d93c61fa70b0d52075497bece8b630b1
Ogalo <ogaloharrison@gmail.com>
2024-03-15 01:32:34 +0300

    Add content

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -30,9 +30,7 @@ const Services = () => {
 
           <Typography variant='h5' fontFamily='karma' pt={2}> UI/UX  Design</Typography>
           <Typography variant='h6' fontFamily='karma' my={1}>
-            Lorem ipsum dolor sit amet consectetur, adipiscing elit neque curae
-            pellentesque dictumst, nam vitae pulvinar cum. Fusce tristique
-            turpis per volutpat erat mollis semper
+          As a seasoned UI/UX designer, I specialize in crafting innovative web and mobile experiences. With a keen eye for detail and a passion for user-centric design, I leverage Figma to bring the clients creative visions to life. I thrive on transforming concepts into seamless digital experiences that leave a lasting impression.
           </Typography>
         </Stack>
         <Stack boxShadow={5} p={2}
@@ -46,9 +44,7 @@ const Services = () => {
 
           <Typography variant='h5' fontFamily='karma' pt={2}> Web Development</Typography>
           <Typography variant='h6' fontFamily='karma' my={1}>
-            Lorem ipsum dolor sit amet consectetur, adipiscing elit neque curae
-            pellentesque dictumst, nam vitae pulvinar cum. Fusce tristique
-            turpis per volutpat erat mollis semper
+          I have a strong understanding of the a full stack MERN web applications from scratch. I have experience with React, CSS, JavaScript, Material UI, Nextjs, MongoDB, Express.JS and Node.
           </Typography>
         </Stack>
         <Stack boxShadow={5} p={2}
@@ -62,9 +58,7 @@ const Services = () => {
 
           <Typography variant='h5' fontFamily='karma' pt={2}> Mobile App</Typography>
           <Typography variant='h6' fontFamily='karma' my={1}>
-            Lorem ipsum dolor sit amet consectetur, adipiscing elit neque curae
-            pellentesque dictumst, nam vitae pulvinar cum. Fusce tristique
-            turpis per volutpat erat mollis semper
+           I excels in crafting cross-platform applications for iOS and Android. Specializing in intuitive UI design and efficient performance optimization, using React Native.
           </Typography>
         </Stack>
         <Stack boxShadow={5} p={2}
@@ -78,9 +72,7 @@ const Services = () => {
 
           <Typography variant='h5' fontFamily='karma' pt={2}> Content Writing</Typography>
           <Typography variant='h6' fontFamily='karma' my={1}>
-            Lorem ipsum dolor sit amet consectetur, adipiscing elit neque curae
-            pellentesque dictumst, nam vitae pulvinar cum. Fusce tristique
-            turpis per volutpat erat mollis semper
+          I possess a deep comprehension of the content creation process and am adept at crafting original content. My expertise spans various forms of writing including articles, blogs, and essays.
           </Typography>
         </Stack>
         <Stack boxShadow={5} p={2}
@@ -94,9 +86,7 @@ const Services = () => {
 
           <Typography variant='h5' fontFamily='karma' pt={2}> Video Editing</Typography>
           <Typography variant='h6' fontFamily='karma' my={1}>
-            Lorem ipsum dolor sit amet consectetur, adipiscing elit neque curae
-            pellentesque dictumst, nam vitae pulvinar cum. Fusce tristique
-            turpis per volutpat erat mollis semper
+          As a dedicated video editor proficient in DaVinci Resolve 18, I specialize in crafting captivating visual narratives. With a keen eye for detail and a passion to transform raw footage into polished masterpieces. My expertise spans various genres, from cinematic shorts to promotional content, ensuring each project exceeds expectations.
           </Typography>
         </Stack>
         <Stack boxShadow={5} p={2}
@@ -110,9 +100,7 @@ const Services = () => {
 
           <Typography variant='h5' fontFamily='karma' pt={2}> Mentorship</Typography>
           <Typography variant='h6' fontFamily='karma' my={1}>
-            Lorem ipsum dolor sit amet consectetur, adipiscing elit neque curae
-            pellentesque dictumst, nam vitae pulvinar cum. Fusce tristique
-            turpis per volutpat erat mollis semper
+          I provide guidance and support to aspiring developers and designers who are seeking to enter the field of software development and designers. If interested kindly reach out.
           </Typography>
         </Stack>
 
