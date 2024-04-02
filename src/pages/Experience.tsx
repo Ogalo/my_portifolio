a98aa2a1e832c737c471967d1610dae91bbf0c0c
Ogalo <ogaloharrison@gmail.com>
2024-04-02 08:05:23 +0300

    Add experience page

---
+++ src/pages/Experience.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography, Stack, Button } from "@mui/material";
+
+
+const Experience = () => {
+  return (
+    <Box sx={{textAlign: 'center', justifyContent: 'center', pt: 30}}>
+      <Typography variant='h4' fontFamily='karma'>Pause! it is in my Resume but will add it here soon for you</Typography></Box>
+  )
+}
+
+export default Experience
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,22 @@
 import Home from './home';
 import Projects from './projects'
+import Blog from './Blog'
+import Experience from './Experience'
+
+import ContactPage from './ContactPage'
+import ServicePage from './ServicePage'
+import AboutPage from './AboutPage'
+
+
 
 
 
 export {
   Home,
   Projects,
+  ContactPage,
+  ServicePage,
+  AboutPage,
+  Blog,
+  Experience,
 };
