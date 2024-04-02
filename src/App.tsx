d67398c3d3e1cd4fe1131bcc8d1eca3656f8f664
Ogalo <ogaloharrison@gmail.com>
2024-04-02 08:01:37 +0300

    Add routes paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import './App.css';
-import { Box } from '@mui/material';
-import { Home } from './pages';
-import { About, Contact, NavBar, Services } from '../src/components';
-import { BrowserRouter,
-  Route, Routes
-} from "react-router-dom";
-import { Projects } from '../src/pages';
-
+import "./App.css";
+import { Box } from "@mui/material";
+import { Home, ContactPage, ServicePage, AboutPage, Blog, Experience } from "./pages";
+import { About, Contact, NavBar, Services } from "../src/components";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Projects } from "../src/pages";
 
 function App() {
   return (
     <>
-    <Routes>
-    <Route path="/" element = {<Home />} />
-    <Route path="/services" element={<Services />} />
-    <Route path="/about" element={<About />} />
-    <Route  path="/contact" element={<Contact />} />
-    <Route  path="/projects" element={<Projects />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<ServicePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/experience" element={<Experience />} />
 
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
     </>
   );
 }
