be711a2a5e7d4e6199e1822ee6996d5bc9cd6bdf
Ogalo <ogaloharrison@gmail.com>
2024-04-02 08:04:12 +0300

    Add blog page

---
+++ src/pages/Blog.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography, Stack, Button } from "@mui/material";
+
+
+const Blog = () => {
+  return (
+    <Box sx={{textAlign: 'center', justifyContent: 'center', pt: 30}}>
+      <Typography variant='h4' fontFamily='karma'>Coming Soon</Typography></Box>
+  )
+}
+
+export default Blog
